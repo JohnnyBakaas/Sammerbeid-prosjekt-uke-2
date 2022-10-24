dbc4452fae891546f3ca1bd3fa93ec4d0822066f
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-10-24 13:02:08 +0200

    Start

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//get Html with this function - Heine
+//make an 2d Array to store the products for shopping cart - Rebecka
+//make shopping cart - Rebecka
+//calculate total price - Heine
